acfd8540c8de5d7ef2fa095366dcff882a426fa1
ralf0018 <ralf.suo@gmail.com>
2025-04-13 12:11:04 +1200

    doing activiy 1

---
+++ week 1 actvities/activity 1.py
@@ -0,0 +1,7 @@
+import numpy as np
+# Create a numpy array of the first 10 positive integers
+arr = np.arange(1,11)
+# print the array
+print(arr)
+# print array shape and data type
+print(arr.shape)
