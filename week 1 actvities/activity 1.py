7fcbce71b68f6db6e070f88a2ccb6b66c192d673
ralf0018 <ralf.suo@gmail.com>
2025-04-17 11:20:18 +1200

    Complete 2 activities

--- week 1 actvities/activity 1.py
+++ week 1 actvities/activity 1.py
@@ -7,8 +7,8 @@ arr = np.arange(1,11)
 print(arr)
 
 # print array shape and data type
-print(arr.shape)
-print(arr.dtype)
+print(f"array shape: {arr.shape}")
+print(f"array data type: {arr.dtype}")
 
 # Multiply each element by 2 and print the result
 newarr = arr*2
--- week 1 actvities/activity 2.py
+++ week 1 actvities/activity 2.py
@@ -1,13 +1,28 @@
 import numpy as np
 
 # Create scores arrey
-scores = np.arry([[78,85,90],[88.79,92],[84,76,88],[90,93,94],[75,80,70]])
+scores = np.array([[78,85,90],
+                  [88,79,92],
+                  [84,76,88],
+                  [90,93,94],
+                  [75,80,70]])
 
 # Calculate and print the average score for each student
+avg_stu = np.mean(scores, axis=1).round(2)
+print(f"the average score of each student: {avg_stu}")
 
 # Calculate and print the average score for each subject
+avg_sub = np.mean(scores, axis=0).round(2)
+print(f"the average score for each subject: {avg_sub}")
 
 # Identify the student (row index) with the highest total score
+# calculate total score for each student
+sum = np.sum(scores,axis=1)
+# find the index of student with highest score
+max = np.argmax(sum)+1
+print(f"student {max} has the highest total score")
 
 # Add 5 bonus points to the third subject for all students
-
+new_scores = scores.copy()
+new_scores[:,2] += 5
+print(new_scores)
