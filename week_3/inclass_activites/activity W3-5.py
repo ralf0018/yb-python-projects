d9044400b4d1118d2700ac9c65cf130f8c1cfa29
ralf0018 <ralf.suo@gmail.com>
2025-04-27 10:09:55 +1200

    update 3 27/4

--- week_3/inclass_activites/activity W3-5.py
+++ week_3/inclass_activites/activity W3-5.py
@@ -5,18 +5,31 @@ class Factorial:
         self.result = 1
     
     #calculate and output the result
-    #if the input is not a integer, return "error", other wise calculate the result
+    #if the input is not valid, return "error", other wise calculate the result
     def cal(self):
-       
-        if self.num!=int(self.num) or self.num<0:
-            return "error"
+        print(self.num-int(self.num))
+        if self.num != int(self.num) or self.num<0:
+            return "invalid input"
         elif self.num == 0 or self.num == 1:
             return 1
         else:
-            for i in range(2,self.num+1,1):
+            for i in range(2,int(self.num)+1,1):
                 self.result *=i
             return self.result
         
+    # check if the input number is prime
+    def isPrime(self):
+        if self.num!=int(self.num) or self.num <=1:
+            return False
+        elif self.num == 2:
+            return True
+        else:
+            for i in range(3,int(self.num**0.5+1)):
+                if self.num % i == 0:
+                    return False
+            return True
 
-f = Factorial(10)
-print(f.cal())
+number = float(input("please input a number: "))
+f = Factorial(number)
+print(f"the factorial result is {f.cal()}")
+print(f"The number is prime: {f.isPrime()}")
