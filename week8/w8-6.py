2f563ef8d9a551e0326eb402b701769d11807104
ralf0018 <ralf.suo@gmail.com>
2025-06-01 12:06:02 +1200

    update week8/w8-6.py

--- week8/w8-6.py
+++ week8/w8-6.py
@@ -1,4 +1,4 @@
-class Person():
+class Person(): # Parrent class
 
   def __init__(self, name, age):
     self.name = name
@@ -7,15 +7,15 @@ class Person():
   def introduce(self):
     return f"Hello, my name is {self.name} and I am {self.age} years old."
   
-class Student(Person):  
+class Student(Person):  # Child class inheriting from Person
   
   def __init__(self, name, age, student_id):
     super().__init__(name, age)  # Call the constructor of the parent class
-    self.student_id = student_id
+    self.student_id = student_id # New attribute
 
   def introduce(self):
     return f"{super().introduce()} My student ID is {self.student_id}."
   
 if __name__ == "__main__":
   student = Student('John', 20, '1234')
-  print(student.introduce())  # Output: Hello, my name is John and I am 20 years old. My student ID is 1234.
+  print(student.introduce())  
